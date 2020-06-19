fe4941bad6ca51ded0e190ae7ec6d227a0ceb4d8
Sven Starcke <sst97@gmx.net>
2020-06-19 15:45:07 +0200

    adds parsing logic for payloads and updates readme

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 Transaction Processor module intended for use within Hyperledger Sawtooth network.
 Used for logging task allocation messages, sent by agents.
+
+## Payload Structure
+
+The transaction processor accepts payloads in the following form: 
+
+```
+agentID|messageType|message|timestamp
+```
+
+- `agentID` refers to the sender by its unique identifier contained in the sent message.
+- `messageType` identifyies the type of the sent message.
+- `message` contains the sent message of type `messagetype`
+- `timestamp` contains the Unix timestamp of reception of the sent message by a monitor.
+
+## Address Structure
+
+Hyperledger Sawtooth networks use 70 bytes for addressing transactions.
+The addresses in this case are calculated from a combination of the transaction namespace `alica_messages` and data contained in the payload.
+The calculation works as follows
+
+```Rust
+hex(sha512("alica_messages"))[0..6]
+    + hex(sha512(agentID + messageType + timestamp))[0..64]
+```
+
+The first 6 bytes of the 70 byte Sawtooth addresses consist of the hash of the transaction namespace.
+The remaining 64 bytes are drawn out of a unique combination of the payload components.
--- src/alica/message.rs
+++ src/alica/message.rs
@@ -2,6 +2,72 @@ use sawtooth_sdk::messages::processor::TpProcessRequest;
 use sawtooth_sdk::processor::handler::{ApplyError, TransactionContext};
 use sha2::Digest;
 
+#[derive(Debug)]
+pub struct Message {
+    agent_id: String,
+    message_type: String,
+    message: Vec<u8>,
+    timestamp: String,
+}
+
+impl Message {
+    pub fn from(bytes: Vec<u8>) -> Result<Message, ApplyError> {
+        let payload = match String::from_utf8(bytes) {
+            Ok(payload) => payload,
+            Err(_e) => {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "Failed to decode payload in UTF8",
+                )))
+            }
+        };
+
+        let mut content = payload.split("|");
+        let agent_id = match content.next() {
+            Some(id) => id,
+            None => {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "No agent ID supplied in payload!",
+                )))
+            }
+        };
+
+        let message_type = match content.next() {
+            Some(t) => t,
+            None => {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "No message type suppliied in payload!",
+                )))
+            }
+        };
+
+        let message = match content.next() {
+            Some(m) => m,
+            None => {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "No message supplied in payload!",
+                )))
+            }
+        };
+
+        let timestamp = match content.next() {
+            Some(t) => t,
+            None => {
+                return Err(ApplyError::InvalidTransaction(String::from(
+                    "No timestamp supplied in payload!",
+                )))
+            }
+        };
+
+        Ok(Message {
+            agent_id: String::from(agent_id),
+            message_type: String::from(message_type),
+            message: message.as_bytes().to_vec(),
+            timestamp: String::from(timestamp),
+        })
+    }
+}
+
+#[derive(Debug)]
 pub struct Handler {
     family_name: String,
     family_versions: Vec<String>,
@@ -48,32 +114,13 @@ impl sawtooth_sdk::processor::handler::TransactionHandler for Handler {
             &request.get_header().get_signer_public_key()[..6]
         );
 
-        let payload = match String::from_utf8(request.get_payload().to_vec()) {
-            Ok(payload) => payload,
-            Err(_e) => {
-                return Err(ApplyError::InvalidTransaction(String::from(
-                    "Failed to decode payload in UTF8",
-                )))
-            }
+        // payload syntax: agent_id|message_type|message|timestamp
+        let message = match Message::from(request.get_payload().to_vec()) {
+            Ok(m) => m,
+            Err(e) => return Err(e),
         };
 
-        /*
-            Messages:
-            {
-                agentId: String,
-                type: MessageTypeEnum,
-                Timestamp: Time,
-                message: Bytes
-            }
-        */
-
-        let message = Message::from(payload);
-        let address = address_for(&message);
-        match context.get_state_entry(address) {
-            Ok()
-        };
-
-        // Addresses: AgentId|MessageType|Timestamp
+        // Addresses: namespace + hex(sha512(AgentId + MessageType + Timestamp))[0..32]
 
         Ok(())
     }
@@ -82,63 +129,117 @@ impl sawtooth_sdk::processor::handler::TransactionHandler for Handler {
 #[cfg(test)]
 mod test {
     use super::*;
-    use sawtooth_sdk::messages::processor::TpProcessRequest;
-    use sawtooth_sdk::processor::handler::{ContextError, TransactionContext, TransactionHandler};
 
-    mockall::mock! {
-        pub Context {}
+    mod messsage {
+        use super::*;
 
-        trait TransactionContext {
-            fn get_state_entries(&self, address: &[String]) -> Result<Vec<(String, Vec<u8>)>, ContextError>;
-            fn set_state_entries(&self, entries: Vec<(String, Vec<u8>)>) -> Result<(), ContextError>;
-            fn delete_state_entries(&self, address: &[String]) -> Result<Vec<String>, ContextError>;
-            fn add_receipt_data(&self, data: &[u8]) -> Result<(), ContextError>;
-            fn add_event(&self, address: String, entries: Vec<(String, String)>, data: &[u8]) -> Result<(), ContextError>;
+        #[test]
+        fn message_is_parsed_if_payload_is_valid() {
+            let id = "id";
+            let message_type = "type";
+            let message_text = "msg";
+            let timestamp = "ts";
+
+            let message_bytes = format!("{}|{}|{}|{}", id, message_type, message_text, timestamp)
+                .as_bytes()
+                .to_vec();
+
+            let message = Message::from(message_bytes).expect("Error parsing payload");
+
+            assert_eq!(message.agent_id, id);
+            assert_eq!(message.message_type, message_type);
+            assert_eq!(message.message, message_text.as_bytes().to_vec());
+            assert_eq!(message.timestamp, timestamp);
+        }
+
+        #[test]
+        fn message_is_not_parsed_if_payload_is_missing_a_timestamp() {
+            let id = "id";
+            let message_type = "type";
+            let message_text = "msg";
+
+            let message_bytes = format!("{}|{}|{}", id, message_type, message_text)
+                .as_bytes()
+                .to_vec();
+
+            Message::from(message_bytes).unwrap_err();
+        }
+
+        #[test]
+        fn message_is_not_parsed_if_payload_is_missing_a_timestamp_and_a_message() {
+            let id = "id";
+            let message_type = "type";
+
+            let message_bytes = format!("{}|{}", id, message_type).as_bytes().to_vec();
+
+            Message::from(message_bytes).unwrap_err();
+        }
+
+        #[test]
+        fn message_is_not_parsed_if_payload_is_missing_a_timestamp_and_a_message_and_a_message_type(
+        ) {
+            let id = "id";
+
+            let message_bytes = format!("{}", id,).as_bytes().to_vec();
+
+            Message::from(message_bytes).unwrap_err();
+        }
+
+        #[test]
+        fn empty_message_is_not_parsed() {
+            let message_bytes = "".as_bytes().to_vec();
+
+            Message::from(message_bytes).unwrap_err();
         }
     }
 
-    #[test]
-    fn apply_with_valid_utf8_payload_succeeds() {
-        let handler = Handler::new();
+    mod handler {
+        use super::*;
+        use sawtooth_sdk::messages::processor::TpProcessRequest;
+        use sawtooth_sdk::processor::handler::{
+            ContextError, TransactionContext, TransactionHandler,
+        };
 
-        let mut request = TpProcessRequest::new();
-        let mut context = MockContext::new();
+        mockall::mock! {
+            pub Context {}
 
-        let mut header = sawtooth_sdk::messages::transaction::TransactionHeader::new();
-        header.set_signer_public_key(String::from("980490840984984"));
-        request.set_header(header);
-        request.set_payload(vec![0x0]);
+            trait TransactionContext {
+                fn get_state_entries(&self, address: &[String]) -> Result<Vec<(String, Vec<u8>)>, ContextError>;
+                fn set_state_entries(&self, entries: Vec<(String, Vec<u8>)>) -> Result<(), ContextError>;
+                fn delete_state_entries(&self, address: &[String]) -> Result<Vec<String>, ContextError>;
+                fn add_receipt_data(&self, data: &[u8]) -> Result<(), ContextError>;
+                fn add_event(&self, address: String, entries: Vec<(String, String)>, data: &[u8]) -> Result<(), ContextError>;
+            }
+        }
 
-        handler.apply(&request, &mut context).unwrap();
-    }
+        #[test]
+        fn apply_with_invalid_utf8_payload_fails_with_apply_error() {
+            let handler = Handler::new();
 
-    #[test]
-    fn apply_with_invalid_utf8_payload_fails_with_apply_error() {
-        let handler = Handler::new();
+            let mut request = TpProcessRequest::new();
+            let mut context = MockContext::new();
 
-        let mut request = TpProcessRequest::new();
-        let mut context = MockContext::new();
+            let mut header = sawtooth_sdk::messages::transaction::TransactionHeader::new();
+            header.set_signer_public_key(String::from("980490840984984"));
+            request.set_header(header);
+            request.set_payload(vec![0xff, 0xff]);
 
-        let mut header = sawtooth_sdk::messages::transaction::TransactionHeader::new();
-        header.set_signer_public_key(String::from("980490840984984"));
-        request.set_header(header);
-        request.set_payload(vec![0xff, 0xff]);
+            handler.apply(&request, &mut context).unwrap_err();
+        }
 
-        handler.apply(&request, &mut context).unwrap_err();
-    }
+        #[test]
+        fn apply_with_validly_structured_payload_succeeds() {
+            let handler = Handler::new();
 
-    #[test]
-    fn apply_with_validly_structured_payload_succeeds() {
-        let handler = Handler::new();
+            let mut request = TpProcessRequest::new();
+            let mut context = MockContext::new();
 
-        let mut request = TpProcessRequest::new();
-        let mut context = MockContext::new();
+            let mut header = sawtooth_sdk::messages::transaction::TransactionHeader::new();
+            header.set_signer_public_key(String::from("980490840984984"));
+            request.set_header(header);
+            request.set_payload("id|type|msg|ts".as_bytes().to_vec());
 
-        let mut header = sawtooth_sdk::messages::transaction::TransactionHeader::new();
-        header.set_signer_public_key(String::from("980490840984984"));
-        request.set_header(header);
-        request.set_payload(String::from("id1|mt|202006162222").as_bytes().to_vec());
-
-        handler.apply(&request, &mut context).unwrap();
+            handler.apply(&request, &mut context).unwrap();
+        }
     }
 }
